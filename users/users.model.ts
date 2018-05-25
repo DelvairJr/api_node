2b809abf23a6289a5664572f785d5adaa66d2882
DelvairJr <delvair.ifms@gmail.com>
2018-05-24 21:20:36 -0400

    validadores padrões - mongoose

--- users/users.model.ts
+++ users/users.model.ts
@@ -1,22 +1,28 @@
 import * as mongoose from 'mongoose'
 
-export interface User extends mongoose.Document{
+export interface User extends mongoose.Document {
     name: string,
     email: string,
-    password: string
+    password: string,
+    maxlength: 80,
+    minlength: 3
 }
 
 const userSchema = new mongoose.Schema({
     name: {
-        type: String
+        type: String,
+        required: true
     },
     email: {
         type: String,
-        unique: true
+        unique: true,
+        match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        required: true
     },
     password: {
         type: String,
-        select: false
+        select: false,
+        required: true 
     }
 })
 
