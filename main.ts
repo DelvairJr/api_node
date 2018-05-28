74ab28cfbbaf527b6c8f81f94fa392d07baecf40
DelvairJr <delvair.ifms@gmail.com>
2018-05-27 22:23:49 -0400

    codificação de restaurants e reviews

--- main.ts
+++ main.ts
@@ -1,11 +1,17 @@
-import {Server} from './Server/server'
+import { Server } from './Server/server'
 import { usersRouter } from './users/users.router'
+import { restaurantRouter } from "./restaurants/restaurant.router";
+import { reviewsRouters } from './reviews/reviews.router';
 
 const server = new Server()
 
-server.bootstrap([usersRouter]).then(server=>{
+server.bootstrap([
+    usersRouter,
+    restaurantRouter,
+    reviewsRouters
+]).then(server => {
     console.log('Server is listening on: ', server.application.address())
-}).catch(error=>{
+}).catch(error => {
     console.log('Server failed start')
     console.error(error)
     process.exit(1)
--- users/users.router.ts
+++ users/users.router.ts
@@ -17,15 +17,10 @@ class UsersRouter extends ModelRouter<User> {
     applyRoutes(application: restify.Server) {
 
         application.get('/users', this.findAll)
-
         application.get('/users/:id', [this.validateId, this.findById])
-
         application.post('/users', this.save)
-
         application.put('/users/:id',  [this.validateId, this.replace])
-
         application.patch('/users/:id',  [this.validateId, this.update])
-
         application.del('/users/:id',  [this.validateId, this.delete])
 
     }
